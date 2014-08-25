afdd58eb87ba9fd0fb86b1dde4d65dba4f6be2f7
skepticfx <ahamed.nafeez@gmail.com>
2014-08-26 03:54:53 +0530

    add handlebar style templating to xss tests

--- security-tests/xss.js
+++ security-tests/xss.js
@@ -1,5 +1,5 @@
 // XSS Commit Checker
-// If the file type if ejs and has <%- in it, 
+// If the file type if ejs and has <%- in it,
 // We assume that it needs to be taken care for manual analysis
 // Just report it using the event.
 
@@ -7,13 +7,13 @@ var events = require('events');
 
 // report as many times as you want
 exports.scan = function(diff, report){
-  
+
   diff.forEach(function(file){
     // Only interested in files with .ejs as extension
     if(file.fileExtension !== 'ejs')
       return;
     file.additions.forEach(function(addition){
-      if(addition.match(/<%-/gi) !== null){
+      if(addition.match(/<%-/gi) !== null || addition.match(/{{{/gi) !=== null){
         report( 'XSS in ' + addition);
       }
     });
